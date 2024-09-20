4dc646ed597614c232f5b41ff1830acb4c9a5899
Loc Le <lpl212757@gmail.com>
2024-09-20 23:09:15 +0700

    format code

--- src/backend/KeyboardLibrary.Identity/Domains/Entities/User.cs
+++ src/backend/KeyboardLibrary.Identity/Domains/Entities/User.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace KeyboardLibrary.Identity.Domains.Entities;
 
-public class User: IdentityUser
+public class User : IdentityUser
 {
-  public string? FirstName { get; set; }
-  public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
 }
