fb8dd0585728a8184159a5e3fbf7803099393311
Loc Le <lpl212757@gmail.com>
2024-10-02 17:34:31 +0700

    feat - Restructure

--- src/backend/KeyboardLibrary.Identity/Domains/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace KeyboardLibrary.Identity.Domains.Entities;
-
-public class User : IdentityUser
-{
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-}
