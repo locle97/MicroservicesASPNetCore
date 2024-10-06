43e2747bfed712472a35b75210c1a6a841b9473e
Loc Le <lpl212757@gmail.com>
2024-10-06 16:57:18 +0700

    feat - reformat book controller

--- src/backend/Library.Book/Controllers/BooksController.cs
+++ src/backend/Library.Book/Controllers/BooksController.cs
@@ -1,27 +1,28 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Library.Book.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class BooksController: ControllerBase
+namespace Library.Book.Controllers
 {
-    public BooksController()
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
     {
-    }
+        public BooksController()
+        {
+        }
 
-    [HttpGet(Name = "Get Books")]
-    public async Task<IActionResult> GetBooks()
-    {
-        string[] books = new string[] { "A", "B" };
-        return Ok(books);
-    }
+        [HttpGet(Name = "Get Books")]
+        public async Task<IActionResult> GetBooks()
+        {
+            string[] books = ["A", "B"];
+            return Ok(books);
+        }
 
-    [Authorize(Policy = "Admin")]
-    [HttpPost(Name = "Update Book")]
-    public async Task<IActionResult> UpdateBook(int id, string name)
-    {
-        return Ok(new {id = id, name = name});
+        [Authorize(Policy = "Admin")]
+        [HttpPost(Name = "Update Book")]
+        public async Task<IActionResult> UpdateBook(int id, string name)
+        {
+            return Ok(new { id, name });
+        }
     }
 }
