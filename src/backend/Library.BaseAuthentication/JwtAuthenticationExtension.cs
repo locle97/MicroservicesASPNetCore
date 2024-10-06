c3fae1cdea514636ef75cb40c1facf1ab8423270
Loc Le <lpl212757@gmail.com>
2024-10-06 16:38:16 +0700

    feat - refactor base authentication

--- src/backend/Library.BaseAuthentication/JwtAuthenticationExtension.cs
+++ src/backend/Library.BaseAuthentication/JwtAuthenticationExtension.cs
@@ -3,48 +3,49 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
-namespace Library.BaseAuthentication;
-
-public static class JwtAuthenticationExtension
+namespace Library.BaseAuthentication
 {
-    public static IServiceCollection AddJwtAuthentication(
-            this IServiceCollection services,
-            string jwtSecretKey,
-            string jwtIssuer)
+    public static class JwtAuthenticationExtension
     {
-        // Argument validation
-        ArgumentNullException.ThrowIfNullOrEmpty(jwtSecretKey);
-        ArgumentNullException.ThrowIfNullOrEmpty(jwtIssuer);
-
-        // Jwt configuration
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    ValidateIssuer = true,
-                    ValidateAudience = false,
-                    ValidIssuer = jwtIssuer,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey))
-                };
-            });
-
-        return services;
-    }
-
-    public static IServiceCollection AddPolicies(
-            this IServiceCollection services)
-    {
-        // Add policy
-        services.AddAuthorization(options =>
+        public static IServiceCollection AddJwtAuthentication(
+                this IServiceCollection services,
+                string jwtSecretKey,
+                string jwtIssuer)
         {
-            options.AddPolicy("Admin", policy =>
-            {
-                policy.RequireRole("ADM");
-            });
-        });
+            // Argument validation
+            ArgumentNullException.ThrowIfNullOrEmpty(jwtSecretKey);
+            ArgumentNullException.ThrowIfNullOrEmpty(jwtIssuer);
 
-        return services;
+            // Jwt configuration
+            _ = services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidIssuer = jwtIssuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey))
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddPolicies(
+                this IServiceCollection services)
+        {
+            // Add policy
+            _ = services.AddAuthorization(static options =>
+            {
+                options.AddPolicy("Admin", static policy =>
+                {
+                    _ = policy.RequireRole("ADM");
+                });
+            });
+
+            return services;
+        }
     }
 }
