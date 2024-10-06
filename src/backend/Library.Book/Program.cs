b2326bb42f409247c77e8bb2ee6ee81e21db9914
Loc Le <lpl212757@gmail.com>
2024-10-06 20:52:08 +0700

    feat - generate authentication for OpenAI Swagger

--- src/backend/Library.Book/Program.cs
+++ src/backend/Library.Book/Program.cs
@@ -3,6 +3,7 @@ using Library.Book.Infrastructure;
 using Library.Book.Infrastructure.Repositories;
 using Library.Book.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace Library.Book;
 
@@ -35,7 +36,33 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            }
+        );
 
         var app = builder.Build();
 
